68de574cf23f0c50c83177742be276b726f33a52
smnan <smuranandi@gmail.com>
2018-09-18 21:42:02 +0200

    Admin page basics are done

---
+++ Tervezes II/IFURETE2/IFURETE2/Controllers/Admin.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IFURETE2.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Users()
+        {
+            return View();
+        }
+
+        public IActionResult Sites()
+        {
+            return View();
+        }
+    }
+}
