9ff34952d1167b0296689bebc87bb1f9f9afa1c5
smnan <smuranandi@gmail.com>
2018-11-04 19:34:24 +0100

    Bootsrap elott

---
+++ Tervezes II/IFURETE2/IFURETE2/Models/Materials.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IFURETE2.Models
+{
+    public class Materials
+    {
+        public int ID { get; set; }
+        public string MaterialName { get; set; }
+        public string Unit { get; set; }
+        public string ShiftingNumber { get; set; }
+
+    }
+}
---
+++ Tervezes II/IFURETE2/IFURETE2/Models/Site.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IFURETE2.Models
+{
+    public class Site
+    {
+        public int ID { get; set; }        
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string NameOfContact { get; set; }
+        public string EmailAddressOfContact { get; set; }
+        public string PhonenumberOfContact { get; set; }
+        public string OpenFrom { get; set; }
+        public string OpenTo { get; set; }
+        public string NumberOfTrucks { get; set; }
+
+    }
+}
